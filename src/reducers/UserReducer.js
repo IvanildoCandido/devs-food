3f3860358d3d982d55c31206a5fe6b5d9ba609f5
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-03-28 17:42:43 -0300

    Configuracao inicial

---
+++ src/reducers/UserReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    name: 'Teste'
+};
+export default (state = initialState, action) => {
+    switch(action.type) {
+        case 'SET_NAME':
+            return {...state, name: action.payload.name};
+    }
+
+    return state;
+}
