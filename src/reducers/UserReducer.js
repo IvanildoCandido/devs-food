87295a61e62a2db4b04f83bb430d252ecf9e45a3
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-03-29 09:24:57 -0300

    Rotas privadas

--- src/reducers/UserReducer.js
+++ src/reducers/UserReducer.js
@@ -1,11 +1,14 @@
 const initialState = {
-    name: 'Teste'
+  token: '',
+  name: 'Teste',
 };
 export default (state = initialState, action) => {
-    switch(action.type) {
-        case 'SET_NAME':
-            return {...state, name: action.payload.name};
-    }
-
-    return state;
-}
+  switch (action.type) {
+    case 'SET_TOKEN':
+      return { ...state, token: action.payload.token };
+    case 'SET_NAME':
+      return { ...state, name: action.payload.name };
+    default:
+      return state;
+  }
+};
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
 import HomeScreen from './pages/HomeScreen';
 import Tela2Screen from './pages/Tela2Screen';
 
@@ -12,6 +13,12 @@ const Routes = () => {
       <Route path="/tela2/:nome">
         <Tela2Screen />
       </Route>
+      <PrivateRoute path="/orders">
+        <h1>Tela de Pedidos</h1>
+      </PrivateRoute>
+      <PrivateRoute path="/profile">
+        <h1>Tela de Perfil</h1>
+      </PrivateRoute>
     </Switch>
   );
 };
