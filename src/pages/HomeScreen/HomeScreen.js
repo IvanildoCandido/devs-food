13a66071bbe3b0b65876aa1d17d26393727fdc50
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-04-01 21:12:50 -0300

    Paginaçao criada

--- src/pages/HomeScreen/HomeScreen.js
+++ src/pages/HomeScreen/HomeScreen.js
@@ -6,6 +6,8 @@ import {
   Container,
   ProductArea,
   ProductList,
+  ProductPaginationArea,
+  ProductPaginationItem,
 } from './HomeScreen.styled';
 import api from '../../api';
 import CategoryItem from '../../components/CategoryItem/CategoryItem';
@@ -16,13 +18,17 @@ export default () => {
   const [headerSearch, setHeaderSearch] = useState('');
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
+  const [totalPages, setTotalPages] = useState(0);
 
   const [activeCategory, setActiveCategory] = useState(0);
+  const [activePage, setActivePage] = useState(0);
 
   const getProducts = async () => {
     const prods = await api.getProducts();
     if (prods.error === '') {
       setProducts(prods.result.data);
+      setTotalPages(prods.result.pages);
+      setActivePage(prods.result.page);
     }
   };
 
@@ -35,8 +41,9 @@ export default () => {
     });
   }, []);
   useEffect(() => {
+    setProducts([]);
     getProducts();
-  }, [activeCategory]);
+  }, [activeCategory, activePage]);
   return (
     <Container>
       <Header search={headerSearch} onSearch={setHeaderSearch} />
@@ -72,6 +79,22 @@ export default () => {
             ))}
           </ProductList>
         </ProductArea>
+      )}
+      {totalPages > 0 && (
+        <ProductPaginationArea>
+          {Array(totalPages)
+            .fill(0)
+            .map((item, index) => (
+              <ProductPaginationItem
+                key={index}
+                active={activePage}
+                current={index + 1}
+                onClick={() => setActivePage(index + 1)}
+              >
+                {index + 1}
+              </ProductPaginationItem>
+            ))}
+        </ProductPaginationArea>
       )}
     </Container>
   );
